4b4a856178cbcaa41d4013d1e6c76f9441b492f1
lxy-Jason <1627962483@qq.com>
2022-08-28 15:32:02 +0800

    test

--- commit.bat
+++ commit.bat
@@ -1,3 +1,11 @@
+@echo off
+echo "-------Begin-------"
+git status
+set /p msg=请输入提交注释:
 git add .
-git commit -m %date%
+git commit -m %msg%
+git pull
 git push
+echo 推送成功：【%msg%】
+echo "--------End!--------"
+pause
